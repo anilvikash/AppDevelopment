f87ac9fa564766f9d899d68291f1a68199611820
anilvikash <120448165+anilvikash@users.noreply.github.com>
2023-10-05 16:30:24 +0530

    Add files via upload

---
+++ day12/jobfair/src/main/java/com/example/demo/Repository/RegisterRepository.java
@@ -0,0 +1,18 @@
+package com.example.demo.Repository;
+
+
+
+
+	import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.demo.Entity.RegisterEntity;
+
+	
+
+	public interface RegisterRepository extends JpaRepository<RegisterEntity,Long> {
+
+		
+
+	}
+
+
