3aad0041efb4a2101c7a03ed84c89acc8dfd99e4
anilvikash <120448165+anilvikash@users.noreply.github.com>
2023-10-11 16:28:27 +0530

    Add files via upload

---
+++ day16/jobfair/src/main/java/com/example/demo/Service/FeedbackService.java
@@ -0,0 +1,29 @@
+package com.example.demo.Service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import com.example.demo.Entity.FeedbackEntity;
+import com.example.demo.Repository.FeedbackRepository;
+
+@Service
+public class FeedbackService {
+
+    @Autowired
+    private FeedbackRepository feedbackRepository;
+
+    public Iterable<FeedbackEntity> getAllFeedback() {
+        return feedbackRepository.findAll();
+    }
+
+    public FeedbackEntity getFeedbackById(Long feedbackId) {
+        return feedbackRepository.findById(feedbackId).orElse(null);
+    }
+
+    public FeedbackEntity saveFeedback(FeedbackEntity feedback) {
+        return feedbackRepository.save(feedback);
+    }
+
+    public void deleteFeedback(Long feedbackId) {
+        feedbackRepository.deleteById(feedbackId);
+    }
+}
